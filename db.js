62445fa2877fb75d0d39f74be9ddeee4ba52c968
lemoscaio <caio_lemos94@hotmail.com>
2022-05-30 07:30:11 -0300

    corrects deploy

--- db.js
+++ db.js
@@ -4,12 +4,13 @@ dotenv.config()
 
 const { Pool } = pg
 
-const db = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: "boardcamp",
-})
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+
+const db = new Pool(databaseConfig)
 
 export default db
